8b98a2fe723e6b761fc85f3dcf3be38265c19764
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-01 18:43:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ class Query:
         if self.type=='add':
             self.name=query[2]
 def read_queries():
-    n = int(input())
+    n=int(input())
     return [Query(input().split()) for i in range(n)]
 def write_responses(result):
     print('\n'.join(result))
 def process_queries(queries):
-    result = []
-    contacts = {}  # Use a dictionary to store the contacts
+    result=[]
+    contacts={}
     for cur_query in queries:
-        if cur_query.type =='add':
-            contacts[cur_query.number] = cur_query.name
-        elif cur_query.type == 'del':
-            contacts.pop(cur_query.number, None)
+        if cur_query.type=='add':
+            contacts[cur_query.number]=cur_query.name
+        elif cur_query.type=='del':
+            contacts.pop(cur_query.number,None)
         else:
-            response = contacts.get(cur_query.number, 'not found')
+            response = contacts.get(cur_query.number,'not found')
             result.append(response)
     return result
-
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
 
